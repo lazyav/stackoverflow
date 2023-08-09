e3e60d7328daaec575fc70615c4bf358eb460828
Abhishek Vyas <106724931+lazyav@users.noreply.github.com>
2023-08-09 10:43:11 +0530

    Add files via upload

---
+++ EmailController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication11.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> SendEmail([FromBody] EmailRequest request)
+        {
+            await Task.Delay(100);
+            string email = request.Email;
+            return Ok(new { Success = true, Message = "Email received and processed successfully." });
+
+        }
+        public class EmailRequest
+        {
+            private string email = "";
+            public string Email { get => email; set => email = value ?? ""; }
+
+        }
+    }
+}
